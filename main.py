9f9f1d5ffa399e9e33dc411c950f98a5eb5ea8ed
janjbyte <jason.nevajuoni@gmail.com>
2025-02-06 20:13:10 +0100

    Added exitable screen function in game

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import *
+
+
+
 def main():
-    if __name__ == "__main__":
+        pygame.init()
         print("Starting asteroids!")
         print(f"Screen width: {SCREEN_WIDTH}")
         print(f"Screen height: {SCREEN_HEIGHT}")
+        
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                   return
+            screen.fill(0)
+            pygame.display.flip()
 
-
-
-main()
+if __name__ == "__main__":
+    main()
