319b414740ceeb2e833b4cfdd4daae1f3a25659e
janjbyte <jason.nevajuoni@gmail.com>
2025-02-06 22:20:54 +0100

    Moving sprite

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
         pygame.init()
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         time = pygame.time.Clock()
-        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         dt = 0
 
         
@@ -18,9 +18,13 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                    return
-                
+            
+            # Added controls to sprite
+            Player.update(player, dt)    
+            
             screen.fill(0)
             player.draw(screen)
+            
             pygame.display.flip()
             
             # FPS limit 60
