22d31433e47ffafe3ed28074c89ef2471c23b543
janjbyte <jason.nevajuoni@gmail.com>
2025-02-12 12:20:04 +0100

    Cleaned code

--- asteroid.py
+++ asteroid.py
@@ -7,7 +7,7 @@ from constants import *
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        self.random_angle = random.uniform(20, 50)
+        
         
         
 
@@ -17,12 +17,19 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+    # new method of splitting asteroids into smaller parts until they reach min radius
     def split(self):
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
-        else:
-            new_asteroid = Asteroid(self.position.x, self.position.y, (self.radius - ASTEROID_MIN_RADIUS))
-            new_asteroid.velocity = self.velocity.rotate(self.random_angle) * 1.2
-            new_asteroid1 = Asteroid(self.position.x, self.position.y, (self.radius - ASTEROID_MIN_RADIUS))
-            new_asteroid1.velocity = self.velocity.rotate(-self.random_angle) * 1.2
+        
+        new_size = self.radius - ASTEROID_MIN_RADIUS
+        random_angle = random.uniform(20, 50)
+        asteroid_1 = self.velocity.rotate(random_angle)
+        asteroid_2 = self.velocity.rotate(-random_angle)
+
+        
+        new_asteroid = Asteroid(self.position.x, self.position.y, new_size)
+        new_asteroid.velocity = asteroid_1 * 1.2
+        new_asteroid = Asteroid(self.position.x, self.position.y, new_size)
+        new_asteroid.velocity = asteroid_2 * 1.2
